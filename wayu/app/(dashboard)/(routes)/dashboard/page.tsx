d4b1c4b01378e0db3a53d42b629221740563319e
roshray <roshanroy759@gmail.com>
2023-07-21 03:46:01 +0530

    setup & Layout🎨

---
+++ wayu/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+    return (
+        <div className="text-6xl text-orange-700">
+            Dashboard Page (Protected)
+        </div> 
+      
+    )
+  }
---
+++ wayu/app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+    return (
+        <div className="text-6xl text-orange-500">
+            Landing Page (unprotected)
+        </div> 
+      
+    )
+  }
